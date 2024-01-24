e7d8949e095c2eb3ae964c743f591f1f56553ea8
Vibes-With-Tushar <131422472+Vibes-With-Tushar@users.noreply.github.com>
2024-01-24 13:38:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { playMusic } from "/listsrc.js";
+import { playMusic } from "./listsrc.js";
 import { currentsong } from '/listsrc.js';
 import { secondsToMMSS } from '/listsrc.js';
 console.log("Script")
